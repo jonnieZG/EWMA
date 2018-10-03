e9eee0922de774546d04a1913d682585226b0529
jonnieZG <jonnieZG@users.noreply.github.com>
2018-10-03 08:59:48 +0200

    Update EwmaT.h

--- EwmaT.h
+++ EwmaT.h
@@ -16,11 +16,11 @@ public:
     /*
     * Creates a filter with a defined initial output.
     */
-    EwmaT(T alpha, int alphaScale);
+    EwmaT(T alpha, unsigned int alphaScale);
     /*
     * Creates a filter with a defined initial output.
     */
-    EwmaT(T alpha, int alphaScale, T initialOutput);
+    EwmaT(T alpha, unsigned int alphaScale, T initialOutput);
 
     void reset();
 
@@ -33,30 +33,30 @@ public:
     T filter(T input);
 
 private:
-    void init(T alpha, int alphaScale, T initialOutput);
+    void init(T alpha, unsigned int alphaScale, T initialOutput);
 
     /*
      * Smoothing factor, in range [0,alphaScale]. Higher the value - less smoothing (higher the latest reading impact).
      */
-    T alpha = 0;
+    T alpha;
     T outputScaled;
-    int alphaScale;
+    unsigned int alphaScale;
     bool hasInitial;
 };
 
 template <typename T>
-EwmaT<T>::EwmaT(T alpha, int alphaScale) {
+EwmaT<T>::EwmaT(T alpha, unsigned int alphaScale) {
     init(alpha, alphaScale, 0);
     this->hasInitial = false;
 }
 
 template <typename T>
-EwmaT<T>::EwmaT(T alpha, int alphaScale, T initialOutput) {
+EwmaT<T>::EwmaT(T alpha, unsigned int alphaScale, T initialOutput) {
     init(alpha, alphaScale, initialOutput);
 }
 
 template <typename T>
-void EwmaT<T>::init(T alpha, int alphaScale, T initialOutput) {
+void EwmaT<T>::init(T alpha, unsigned int alphaScale, T initialOutput) {
     this->alpha = alpha;
     this->alphaScale = alphaScale;
     this->outputScaled = initialOutput * alphaScale;
